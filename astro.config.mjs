1ade788a932ae346808fe82ae33b99240635e7e0
Ping5-1 <wyp_hqu2017@163.com>
2024-07-31 15:53:33 +0800

    fix: 配置根目录

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://Ping5-1.github.io",
-  base: "/blog",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(),
